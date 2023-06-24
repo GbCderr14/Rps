19ab8e5ac85dc660813803051cb5d821f3b4b530
GbCderr14 <114488917+GbCderr14@users.noreply.github.com>
2023-06-24 15:00:12 +0530

    made some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from './components/landingPage'
 import { Route, Routes } from 'react-router'
