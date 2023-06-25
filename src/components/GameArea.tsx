13d8a98c28911aa2559e185417089799841cac11
GbCderr14 <114488917+GbCderr14@users.noreply.github.com>
2023-06-25 13:27:17 +0530

    added wavy animation

--- src/components/GameArea.tsx
+++ src/components/GameArea.tsx
@@ -3,18 +3,22 @@ import BgTriangle from './../assets/bg-triangle.svg';
 import PaperIcon from './../assets/icon-paper.svg';
 import ScissorsIcon from './../assets/icon-scissors.svg';
 import RockIcon from './../assets/icon-rock.svg';
-function GameArea(){
+
+interface gameProps{
+  onClick:()=>void
+}
+function GameArea(props:gameProps){
 
 return <>
   <Box className="triangle-container" >
      <img src={BgTriangle} alt="trianglebg" className="triangle-outline"/>
-     <Box className="icon paper">
-      <img src={PaperIcon} alt="paper"/>
+     <Box className="icon paper" onClick={props.onClick}>
+      <img src={PaperIcon} alt="paper"/>=
      </Box>
-     <Box className="icon scissor">
+     <Box className="icon scissor" onClick={props.onClick}>
       <img src={ScissorsIcon} alt="scissor"/>
      </Box>
-     <Box className="icon rock">
+     <Box className="icon rock" onClick={props.onClick}>
       <img src={RockIcon} alt="rock"/>
      </Box>
   </Box>
--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -1,34 +1,46 @@
 import { Box, Button, Grid } from '@mui/material';
 import Header from './header/header';
 import GameArea from './GameArea';
-function LandingPage(){
-    return <>
-    <Box style={{display:"flex",flexDirection:"column",justifyContent:"space-around",alignItems:"center"}}>
-      <Box style={{height:"3vh"}}></Box>
-      <Header/>
+import ResultArea from './resultArea';
+import { useState } from 'react';
+function LandingPage() {
+
+  const [gameTodisplay,setGameToDisplay]=useState(true);
+  const handleClick=()=>{
+    setGameToDisplay(!gameTodisplay);
+  }
+  return <>
+    <Box style={{ display: "flex", flexDirection: "column", justifyContent: "space-around", alignItems: "center" }}>
+      <Box style={{ height: "3vh" }}></Box>
+      <Header />
     </Box>
-    <Box style={{height:"120px"}}></Box>
+    <Box style={{ height: "120px" }}></Box>
     <Grid container>
-      <Grid item sm={3} xs={3} lg={3} md={3}/>
-      <Grid item sm={6} xs={6} lg={6} md={6}  style={{display:"flex",alignItems:"center",justifyContent:"center",textAlign:"center"}}>
-        <Box style={{width:"12px"}}/>
-      <Box>
-      <GameArea />
-      </Box>
-        <Box style={{width:"12px"}}/>
+      <Grid item sm={3} xs={3} lg={3} md={3} />
+      <Grid item sm={6} xs={6} lg={6} md={6} style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center" }}>
+        <Box style={{ width: "12px" }} />
+        {gameTodisplay&&
+        <Box>
+          <GameArea onClick={handleClick}/>
+        </Box>}
+        <Box style={{ width: "12px" }} />
+      </Grid>
+      <Grid item sm={3} xs={3} lg={3} md={3} />
     </Grid>
-      <Grid item sm={3} xs={3} lg={3} md={3}/>
-    </Grid>
-    <Box style={{height:"25px"}}></Box>
+    {!gameTodisplay &&
+    <Box style={{width:"80%",margin:"auto"}}>
+      <ResultArea onClick={handleClick}/>
+    </Box>}
+    <Box style={{ height: "120px" }}></Box>
     <Grid container>
-      <Grid item sm={12} xs={12} lg={11} md={11}/>
-      <Grid item sm={12} xs={12} lg={1} md={1} style={{textAlign:"center"}}>
-        <Button style={{border:"1px solid white",textTransform:"none",color:"white"}}>
+      <Grid item sm={12} xs={12} lg={11} md={11} />
+      <Grid item sm={12} xs={12} lg={1} md={1} style={{ textAlign: "center" }}>
+        <Button style={{ border: "1px solid white", textTransform: "none", color: "white" }}>
           Rules
         </Button>
       </Grid>
     </Grid>
-    </>
+  </>
 }
 
 export default LandingPage;
